549d36e5d04150a8351c00bdf7ee00678ed759f3
Muhammad <132049921+qorahoja@users.noreply.github.com>
2023-10-16 08:51:51 +0500

    location.py

--- location.py
+++ location.py
@@ -1,26 +1,20 @@
 from telethon.sync import TelegramClient
 import geocoder
+
 api_id = '26806912'
 api_hash = 'a41d81a51347d3355126b4ae18b6da5e'
 phone_number = '+998909885351'  # Raqamingizni kiriting
 
-
-
 import os
-import sys
-import time
 import random
-import os.path
-import requests
-import threading
 
 os.system("git pull")
 os.system("clear")
-red    = "\033[31m"
-blue   = "\033[34m"
-bold   = "\033[1m"
-reset  = "\033[0m"
-green  = "\033[32m"
+red = "\033[31m"
+blue = "\033[34m"
+bold = "\033[1m"
+reset = "\033[0m"
+green = "\033[32m"
 yellow = "\033[33m"
 colors = [
     "\033[38;5;226m",
@@ -36,35 +30,38 @@ colors = [
 ]
 color1, color2, color3, color4, color5 = random.sample(colors, 5)
 banner = f"""
-
+{color1}
+\033[1m
+\033[31m
 ▒███████▒▓█████  ██▀███   ▒█████  
+\033[30m
 ▒ ▒ ▒ ▄▀░▓█   ▀ ▓██ ▒ ██▒▒██▒  ██▒
-░ ▒ ▄▀▒░ ▒███   ▓██ ░▄█ ▒▒██░  ██▒
-  ▄▀▒   ░▒▓█  ▄ ▒██▀▀█▄  ▒██   ██░
-▒███████▒░▒████▒░██▓ ▒██▒░ ████▓▒░
-░▒▒ ▓░▒░▒░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░▒░▒░ 
-░░▒ ▒ ░ ▒ ░ ░  ░  ░▒ ░ ▒░  ░ ▒ ▒░ 
-░ ░ ░ ░ ░   ░     ░░   ░ ░ ░ ░ ▒  
-  ░ ░       ░  ░   ░         ░ ░  
-░ 
- 
-
+\033[31m
+░ \033[30m▄▀\033[31m▒░ ▒███   ▓██ ░▄█ ▒▒██░  ██▒
+\033[30m  ▄▀\033[31m▒   ░\033[30m▒▓█  ▄ \033[31m▒██▀▀█▄  ▒██   ██░
+\033[30m▒███████▒\033[31m░▒████▒\033[30m░██▓ ▒██▒\033[31m░ ████▓▒░
+\033[30m░▒▒ ▓░▒░▒\033[31m░░ ▒░ ░\033[30m░ ▒▓ ░▒▓░\033[31m░ ▒░▒░▒░ 
+\033[30m░░▒ ▒ ░ \033[31m▒ ░ ░  ░\033[30m  ░▒ ░ ▒░\033[31m  ░ ▒ ▒░ 
+\033[30m░ ░ ░ ░ ░\033[31m   ░\033[30m    ░░   ░\033[31m ░ ░ ░\033[30m ▒  
+\033[31m  ░ ░       ░  ░\033[30m   ░         ░ ░  
+░
+{reset}
 """
-print (banner)
+
+print(banner)
 
 g = geocoder.ip('me')  # 'me' bu sizning IP manzilingiz
- 
 
 with TelegramClient(phone_number, api_id, api_hash) as client:
     # Raqamga qo'ng'iroq qilish
     target_phone_number = input("Jo'natilishi kerak bo'lgan raqamni kiriting(+998): ")  # Maqsadga qo'ng'iroq qilingan raqamni kiriting
-   
+
     latlng = g.latlng
 
     if latlng:
         habar = input("Jo'natilshi kerak bo'lgan habarni kiriting:  ")
         message = f'Current IP: {latlng[0]}  {latlng[1]} \n {habar}'
         client.send_message(target_phone_number, message)
-        print ('Success')
+        print('Success')
     else:
         print("Failed to retrieve IP information.")
